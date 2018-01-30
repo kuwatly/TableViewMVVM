d6fe88d5005ee49cf854a9c66fcf1d7281c184d0
Iyad Kuwatly <ikuwatly@gmail.com>
2018-01-29 21:40:38 -0500

    Add tableView textField and button

--- TableViewMVVM/ViewController.swift
+++ TableViewMVVM/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableViewItems: UITableView!
+    @IBOutlet weak var textFieldNewItem: UITextField!
+    
+    @IBAction func onAddItem(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
